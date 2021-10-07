71d11aa3259e6b0a5f143eaf9e32ab8983f799e8
Ditya <68582042+DityaDeveloper@users.noreply.github.com>
2021-10-07 11:29:22 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1633579849,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/playertank-sheet0.png",
+		"images/playerhealtbar-sheet0.png",
+		"images/enemyhealtbar-sheet0.png",
+		"images/enemytank-sheet0.png",
+		"images/enemytank-sheet1.png",
+		"images/spinner-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/shell-sheet0.png",
+		"images/tiledbackground.png",
+		"images/tilemap.png",
+		"images/tilemap2.png",
+		"images/shellexplosion-sheet0.png",
+		"images/shellexplosion-sheet1.png",
+		"images/explosion-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
